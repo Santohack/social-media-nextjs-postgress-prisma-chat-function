20b442eda4a8eac053a82ae2cfc247ecfe8a3028
santohack <santohack238@gmail.com>
2024-10-27 09:45:03 +0530

    responsvie done

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -5,14 +5,28 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-10 bg-card shadow-sm">
-      <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
+    <div className="w-full   lg:hidden block">
+    <div className=" flex  w-full items-center justify-between  max-w-7xl flex-wrap px-3  py-3">
         <Link href="/" className="text-2xl font-bold text-primary">
         <span className="text-primary">S</span>hare
+        <span className="text-primary">🤫</span>
+        </Link>
+        <div className="w-44">
+        <SearchField />
+        </div>
+        <UserButton  />
+      </div>
+    </div>
+     <div className="w-full  lg:block hidden">
+     <div className="mx-auto  max-w-7xl flex-wrap items-center flex justify-center gap-5 px-5 py-3">
+        <Link href="/" className="text-2xl font-bold text-primary">
+        <span className="text-primary">S</span>hareb
         <span className="text-primary">🤫</span>
         </Link>
         <SearchField />
         <UserButton className="sm:ms-auto" />
       </div>
+     </div>
     </header>
   );
 }
